68b153f20f077092a2cef5e07c373dc998e4c554
MrSedan <elpashevs@bk.ru>
2022-10-08 11:16:03 +0300

    Keyboard interaction

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ token = config['TOKEN']
 
 bot = telebot.TeleBot(token)
 
-def gen_markup_for_buy():
+def gen_markup_for_buy(msg_id):
     markup = types.InlineKeyboardMarkup()
     markup.row_width = 1
     # ⬅️➡️
     left_but = types.InlineKeyboardButton('⬅️', callback_data='prev')
     right_but = types.InlineKeyboardButton('➡️', callback_data='next')
-    back_but = types.InlineKeyboardButton('Назад', callback_data='/start')
+    back_but = types.InlineKeyboardButton('Назад', callback_data=f'/start {msg_id}')
     markup.row(left_but, right_but)
     markup.add(back_but)
     return markup
@@ -23,21 +23,33 @@ def start_message(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     buy_tick_but = types.KeyboardButton('Купить билеты!')
     markup.add(buy_tick_but)
-    bot.send_message(message.chat.id, 'Добро пожаловать в билетную кассу театра имени А.Боба! Выберете действие, что вы хотите совершить.', reply_markup=gen_markup_for_buy())
+    bot.send_message(message.chat.id, 'Добро пожаловать в билетную кассу театра имени А.Боба!')
+    bot.send_message(message.chat.id, 'Выберете действие, что вы хотите совершить.', reply_markup=markup)
 
 @bot.message_handler(func=lambda message: True)
 def reply_markup_reaction(message):
     if message.text == 'Купить билеты!':
 
-        bot.send_message(message.chat.id, 'Выводится список ближайших концертов...',
+        msg = bot.send_message(message.chat.id, 'Выводится список ближайших концертов...',
                          reply_markup=types.ReplyKeyboardRemove())
-        bot.send_message(message.chat.id, 'Фиг те, а не билеты!', reply_markup=markup)
+        bot.send_message(message.chat.id, 'Фиг те, а не билеты!', reply_markup=gen_markup_for_buy(msg.message_id))
+
+i = 1
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
-    i = 1
-    if call.data == '/start':
-        bot.answer_callback_query(call.id, 'aboba', show_alert=False)
+    global i
+    if call.data.startswith('/start'):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        buy_tick_but = types.KeyboardButton('Купить билеты!')
+        markup.add(buy_tick_but)
+        msg_id = call.data.split()[1]
+        bot.edit_message_text('Добро пожаловать в билетную кассу театра имени А.Боба!',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=None)
+        bot.send_message(call.message.chat.id, 'Выберете действие, что вы хотите совершить.',  reply_markup=markup)
+        bot.delete_message(call.message.chat.id, int(msg_id))
     elif call.data == 'prev':
         i-=1
         bot.edit_message_text(f'Message: {i}', call.message.chat.id, call.message.message_id, reply_markup=gen_markup_for_buy())
