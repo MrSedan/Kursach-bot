d6f53faa40d3ae508bf74931ef90c8886bfdcff1
MrSedan <elpashevs@bk.ru>
2022-12-13 17:04:59 +0300

    Encoding and default values in Concerts table for testing

--- initial.sql
+++ initial.sql
@@ -1,3 +1,4 @@
+PRAGMA encoding = "UTF-8";
 DROP TABLE IF EXISTS "Concerts";
 CREATE TABLE "Concerts" (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,3 +18,6 @@ CREATE TABLE "Tickets" (
         ON DELETE CASCADE
         MATCH SIMPLE
 );
+INSERT INTO Concerts (id, name, date, time) VALUES (1, 'Вагнер', '14.09.2023', '14:00');
+INSERT INTO Concerts (id, name, date, time) VALUES (2, 'Моцарт', '13.02.2023', '9:00');
+INSERT INTO Concerts (id, name, date, time) VALUES (3, 'Тест', '10.04.2023', '12:00');
